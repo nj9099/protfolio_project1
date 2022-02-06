0c29946620192f6df5210737fe1307e26d74681a
noeljp <noeljp98@gmail.com>
2022-02-05 19:21:16 -0500

    Create DIM_Customer.sql

---
+++ queries/DIM_Customer.sql
@@ -0,0 +1,37 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT 
+  c.CustomerKey as CustomerKey, 
+  --      ,[GeographyKey]
+  --      ,[CustomerAlternateKey]
+  --      ,[Title]
+  c.FirstName AS FirstName, 
+  --      ,[MiddleName], 
+  c.LastName AS [Last Name], 
+  c.FirstName + ' ' + c.LastName AS [Full Name],
+  --      ,[NameStyle]
+  --      ,[BirthDate]
+  --      ,[MaritalStatus]
+  --      ,[Suffix]
+ 
+  CASE c.Gender WHEN 'M' THEN 'MALE' WHEN 'F' THEN 'FEMALE' END AS Gender, 
+  --      ,[EmailAddress]
+  --      ,[YearlyIncome]
+  --      ,[TotalChildren]
+  --      ,[NumberChildrenAtHome]
+  --      ,[EnglishEducation]
+  --      ,[SpanishEducation]
+  --      ,[FrenchEducation]
+  --      ,[EnglishOccupation]
+  --     ,[SpanishOccupation]
+  --      ,[FrenchOccupation]
+  --      ,[HouseOwnerFlag]
+  --    ,[NumberCarsOwned]
+  --   ,[AddressLine1]
+  --   ,[AddressLine2]
+  --   ,[Phone]
+  c.datefirstpurchase AS DateFirstPurchase, 
+  --,[CommuteDistance]
+  g.City AS [Customer City] 
+FROM 
+  dbo.DimCustomer as c 
+  LEFT JOIN dbo.DimGeography as g ON g.GeographyKey = c.GeographyKey
